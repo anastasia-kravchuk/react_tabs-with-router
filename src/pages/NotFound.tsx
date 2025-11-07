eac50cdb5c4016fd57f7635278ddab36fd042628
Anastasia <ahinata1717@gmail.com>
2025-11-07 14:25:06 +0200

    removed semicolumn

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ export const NotFound = () => {
   return (
     <div className="section">
       <div className="container">
-        <h1 className="title">Page not found</h1>;
+        <h1 className="title">Page not found</h1>
       </div>
     </div>
   );
